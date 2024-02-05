738218b33f5f8c66f5a34a62542aa4054fccc0ac
Aditya-Aryan-design <113804917+Aditya-Aryan-design@users.noreply.github.com>
2024-02-06 00:38:36 +0530

    changed pointercontrol to cameracontrol

--- components/FPV.tsx
+++ components/FPV.tsx
@@ -1,9 +1,7 @@
-import { useThree } from "@react-three/fiber";
-import { PointerLockControls } from "@react-three/drei";
+import { CameraControls } from "@react-three/drei";
 
 
 export default ()=>{
-    const {camera,gl} = useThree();
 
-    return <PointerLockControls args={[camera,gl.domElement]}/>
+    return <CameraControls />
 }
